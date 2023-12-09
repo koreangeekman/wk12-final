5eb4c4d9d94d069c039a656b223339bd1d2c1d70
DJ <koreangeekman@gmail.com>
2023-12-09 03:01:17 -0700

    color codes

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,5 +26,11 @@ export default {
 
 :root {
   --main-height: calc(100vh - 64px);
+  --color-1: #F9F6FA;
+  --color-2: #FEF6F0;
+  --color-3: #E9D8D6;
+  --color-4: #DED6E9;
+  --color-5: #877A8F;
+  --color-6: #2D2D2D;
 }
 </style>
